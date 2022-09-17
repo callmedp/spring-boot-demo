52cd9fa938d5e36e3b449af15858c0186d84a3e4
Carat^ <divyanshu3490@gmail.com>
2022-09-17 22:37:24 +0530

    section 3 completed

--- src/main/java/com/example/demo/controller/HelloWorldController.java
+++ src/main/java/com/example/demo/controller/HelloWorldController.java
@@ -1,12 +1,22 @@
 package com.example.demo.controller;
 
 import com.example.demo.bean.HelloWorldBean;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Locale;
+
 @RestController
 public class HelloWorldController {
+
+    private MessageSource messageSource;
+
+    public HelloWorldController(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
 
     @GetMapping(path="/hello")
     public String helloWorld() {
@@ -21,5 +31,12 @@ public class HelloWorldController {
     @GetMapping(path="/hello-world/path-variable/{info}")
     public HelloWorldBean helloWorldPathVariable(@PathVariable String info) {
         return new HelloWorldBean("Hello World", info);
+    }
+
+    @GetMapping(path="/hello-internationalized")
+    public String helloWorldInternationalized() {
+        Locale locale = LocaleContextHolder.getLocale();
+        String message = messageSource.getMessage("good.morning.message", null, "Default Message", locale);
+        return message;
     }
 }
